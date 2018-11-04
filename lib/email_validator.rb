9ba009f859262d0b37891e215998dcabce6061c3
karolina oska <karolina.oska@netguru.pl>
2018-11-04 22:36:02 +0100

    Validate email with each validator

--- lib/email_validator.rb
+++ lib/email_validator.rb
@@ -1,5 +1,18 @@
-class EmailValidator
-  def self.valid?(email)
-    !!(email =~ /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/)
+require 'active_model'
+
+class EmailValidator < ActiveModel::EachValidator
+
+  def validate_each(record, attribute, value)
+    unless self.class.valid?(value)
+      record.errors.add(attribute, options[:message] || :invalid_email_format)
+    end
+  end
+
+  def self.valid?(value)
+    !!(value =~ valid_email_regex)
+  end
+
+  def self.valid_email_regex
+    /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   end
 end
